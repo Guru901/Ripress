2a5e2aa0af7ee47d43a126f4703bf570044dd9f5
Guru901 <avinashkaur842@gmail.com>
2025-03-12 00:34:13 +0530

    Refactor from_actix_request method to improve query parsing and storage

--- src/lib.rs
+++ src/lib.rs
@@ -238,13 +238,22 @@ impl HttpRequest {
     }
 
     fn from_actix_request(req: &actix_web::HttpRequest) -> Self {
+        let mut queries = HashMap::new();
+        if let Some(query_string) = req.query_string().split("?").next() {
+            query_string.split("&").for_each(|pair| {
+                if let Some((key, value)) = pair.split_once("=") {
+                    queries.insert(key.to_string(), value.to_string());
+                }
+            });
+        }
+
         HttpRequest {
             params: req
                 .match_info()
                 .iter()
                 .map(|(k, v)| (k.to_string(), v.to_string()))
                 .collect(),
-            queries: HashMap::new(),
+            queries,
         }
     }
 }
