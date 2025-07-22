6216c5cd771d44d5a8a252c7965a66edfd7b332b
Gurvinder Singh <avinashkaur842@gmail.com>
2025-07-23 02:28:30 +0530

    Enhance HttpResponse struct to include headers management. Add methods for setting content type and custom headers, improving response flexibility.

--- src/res/mod.rs
+++ src/res/mod.rs
@@ -1,11 +1,14 @@
 use crate::types::{ResponseContentBody, ResponseContentType};
 use actix_web::Responder;
+use actix_web::http::header::{HeaderName, HeaderValue};
 use serde::Serialize;
+use std::collections::HashMap;
 
 pub struct HttpResponse {
     body: ResponseContentBody,
     content_type: ResponseContentType,
     status_code: u16,
+    headers: HashMap<String, String>,
 }
 
 impl HttpResponse {
@@ -14,6 +17,7 @@ impl HttpResponse {
             status_code: 200,
             body: ResponseContentBody::TEXT(String::new()),
             content_type: ResponseContentType::TEXT,
+            headers: HashMap::new(),
         }
     }
     pub fn text<T: Into<String>>(mut self, text: T) -> Self {
@@ -33,8 +37,20 @@ impl HttpResponse {
         self
     }
 
+    pub fn content_type(mut self, content_type: &str) -> Self {
+        self.headers
+            .insert("Content-Type".to_string(), content_type.to_string());
+        self
+    }
+    pub fn set_header(mut self, header_name: &str, header_value: &str) -> Self {
+        self.headers
+            .insert(header_name.to_string(), header_value.to_string());
+
+        self
+    }
+
     pub fn to_responder(self) -> actix_web::HttpResponse {
-        let actix_res = actix_web::http::StatusCode::from_u16(self.status_code as u16)
+        let mut actix_res = actix_web::http::StatusCode::from_u16(self.status_code as u16)
             .map(|status| match self.body {
                 ResponseContentBody::JSON(json) => actix_web::HttpResponse::build(status)
                     .content_type("application/json")
@@ -46,6 +62,13 @@ impl HttpResponse {
             .unwrap_or_else(|_| {
                 actix_web::HttpResponse::InternalServerError().body("Invalid status code")
             });
+
+        self.headers.iter().for_each(|(key, value)| {
+            actix_res.headers_mut().append(
+                HeaderName::from_bytes(key.as_bytes()).unwrap(),
+                HeaderValue::from_str(value).unwrap(),
+            )
+        });
 
         return actix_res;
     }
