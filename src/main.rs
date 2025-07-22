e38b968c5330a24c80c6859cbb8fb0b35e18497f
Gurvinder Singh <avinashkaur842@gmail.com>
2025-07-23 01:57:26 +0530

    Refactor text_handler and from_actix_request functions to remove unused parameters, improving code clarity and maintainability.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use ripress_again::app::Ripress;
 use ripress_again::req::HttpRequest;
 use ripress_again::res::HttpResponse;
-use serde_json::json;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -13,6 +12,6 @@ async fn main() -> std::io::Result<()> {
         .await
 }
 
-async fn text_handler(req: HttpRequest, res: HttpResponse) -> HttpResponse {
+async fn text_handler(_req: HttpRequest, res: HttpResponse) -> HttpResponse {
     return res.text("Hello, World!");
 }
--- src/req/mod.rs
+++ src/req/mod.rs
@@ -1,7 +1,7 @@
 pub struct HttpRequest {}
 
 impl HttpRequest {
-    pub fn from_actix_request(req: actix_web::HttpRequest) -> Self {
+    pub fn from_actix_request(_req: actix_web::HttpRequest) -> Self {
         HttpRequest {}
     }
 }
